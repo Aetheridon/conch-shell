dfec8875ee33b09ca93cb3bb2ffa0c5cc172256b
Aetheridon <lewisvr@protonmail.com>
2024-10-05 17:53:07 +0100

    basic io

--- src/conch.c
+++ src/conch.c
@@ -1,8 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+int sh_loop(void);
 
 int main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
+    sh_loop();
     return 0;
+}
+
+int sh_loop(void)
+{
+    while (1)
+    {
+        char cmd[100];
+
+        printf("\n$ ");
+
+        if (fgets(cmd, sizeof(cmd), stdin) == NULL)
+        {
+            perror("fgets failed to read from stdin.\n");
+            return 1;
+        }
+
+        cmd[strcspn(cmd, "\n")] = 0; 
+
+        if (strcmp(cmd, "exit") == 0)
+        {
+            return 0;
+        }
+
+        printf("%s", cmd);
+    }
+
+    return 0;
+
 }
