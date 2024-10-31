a8e936dba23fbba913cc5d855547d3c93f9a0ab2
Aetheridon <lewisvr@protonmail.com>
2024-10-31 18:49:02 +0000

    Added basic i/o redirection

--- src/forkexec.c
+++ src/forkexec.c
@@ -4,26 +4,78 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #include "forkexec.h"
 
-void tokenize(char **args, char *cmd)
-{
-    int arg_count = 0;    
-
+void tokenize(char **args, char *cmd) {
+    int arg_count = 0;
     char *token = strtok(cmd, " ");
+    
+    while (token != NULL && arg_count < MAX_ARGS - 1)
+    {
+        if (strchr(token, '>') != NULL)
+        {
+            char *pos = strstr(token, ">>");
 
-    while (token != NULL && arg_count < MAX_ARGS - 1) {
-        args[arg_count] = token;
-        arg_count++;
-        token = strtok(NULL, " "); // proceeds to point to the next token.
+            if (pos == token)
+            {
+                args[arg_count++] = ">>";
+                token = strtok(NULL, " ");
+                continue;
+            }
+        
+            pos = strchr(token, '>');
+
+            if (pos == token)
+            {
+                args[arg_count++] = ">";
+                token = strtok(NULL, " ");
+                continue;
+            }
+        }
+        
+        args[arg_count++] = token;
+        token = strtok(NULL, " ");
     }
-
+    
     args[arg_count] = NULL;
+}
+
+
+void parse_redirection(char **args, char **outfile, int *redirect_type)
+{
+    *redirect_type = 0;
+    *outfile = NULL;
+
+    for (int i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], ">") == 0)
+        {
+            *redirect_type = REDIRECT_OUT;
+            *outfile = args[i + 1];
+            args[i] = NULL;
+            break;
+        }
+
+        else if (strcmp(args[i], ">>") == 0)
+        {
+            *redirect_type = REDIRECT_APPEND;
+            *outfile = args[i + 1];
+            args[i] = NULL;
+            break;
+        }
+    }
 }
 
 void startup_cmd(char **args)
 {
+    char *outfile;
+    int redirect_type;
+
+    parse_redirection(args, &outfile, &redirect_type);
+
     pid_t pid = fork();
 
     if (pid < 0)
@@ -32,8 +84,39 @@ void startup_cmd(char **args)
         exit(1);
     }
 
-    if (pid == 0) // fork returns 0 if we're the child process
-    {   
+    if (pid == 0) { // Child process
+
+        if (redirect_type != 0)
+        {
+            int flags, fd;
+                
+            if (redirect_type == REDIRECT_OUT)
+            {
+                flags = O_WRONLY | O_CREAT | O_TRUNC;
+            }
+
+            else
+            {
+                flags = O_WRONLY | O_CREAT | O_APPEND;
+            }
+                
+            fd = open(outfile, flags, 0644);
+
+            if (fd < 0)
+            {
+                perror("Failed to open output file");
+                exit(1);
+            }
+                
+            if (dup2(fd, STDOUT_FILENO) < 0)
+            {
+                perror("Failed to redirect stdout");
+                exit(1);
+            }
+
+            close(fd);
+        }
+        
         if (execvp(args[0], args) == -1)
         {
             perror("Failed to execute command\n");
@@ -41,7 +124,7 @@ void startup_cmd(char **args)
         }
     }
 
-    else
+    else 
     {
         wait(NULL);
     }
--- src/forkexec.h
+++ src/forkexec.h
@@ -4,6 +4,8 @@
 #define MAX_CMD_LEN 256 // Max length of whole command, whitespaces etc.
 #define MAX_ARGS 64 // Max amount of arguments within the command.
 #define MAX_ARG_LEN 256 // Max length of an argument within the command.
+#define REDIRECT_OUT 1 // >
+#define REDIRECT_APPEND 2 // >>
 
 void tokenize(char **args, char *cmd);
 void startup_cmd(char **args);
